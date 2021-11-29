25950491d23408b271a7a1574639db49bd0b0b68
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-11-29 16:26:45 +0100

    added CardDeck script

---
+++ Assets/Scripts/Cards/CardDeck.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO make this into a factory pattern or somethign similar, that creates and stores a stack of cards
+public class CardDeck : MonoBehaviour
+{
+    private Stack cardStack = new Stack();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
