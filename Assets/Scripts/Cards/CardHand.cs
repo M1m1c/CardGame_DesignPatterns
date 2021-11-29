336c80b15dfa1c21d52f5b2291390a5e0635cc67
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-11-29 16:27:00 +0100

    Added CardHand Script

---
+++ Assets/Scripts/Cards/CardHand.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardHand : MonoBehaviour
+{
+
+    //TODO needs a list of cards,
+    //a way of highlighting cards that the mouse pases over,
+    //a way of selecting cards in hand,
+    //a way of playing cards
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
