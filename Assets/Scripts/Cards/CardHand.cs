bdfafc496a5f8b90cbe1793a901d1aee85ca2b13
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-02 16:03:05 +0100

    added funciton for drawing cards, psoitioning them correctly and highlighting them

--- Assets/Scripts/Cards/CardHand.cs
+++ Assets/Scripts/Cards/CardHand.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class CardHand : MonoBehaviour
 {
 
+    public CardDeck cardDeck;
+
+    private int currentHandCount = 0;
+
+    private float cardXOfffest = 8.5f;
+    private float cardXStartPos = -17f;//-21.25f;
+
+    private CardBase[] heldCards = new CardBase[5];
+    private CardBase HighLightedCard = null;
+
     //TODO needs a list of cards,
     //a way of highlighting cards that the mouse pases over,
     //a way of selecting cards in hand,
@@ -13,12 +23,58 @@ public class CardHand : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        DrawCardsPhase();
+    }
+
+    private void DrawCardsPhase()
+    {
+        while (currentHandCount < heldCards.Length)
+        {
+            if (DrawCard(currentHandCount))
+            {
+                currentHandCount++;
+            }
+        }
+        ReorganizeHeldCardPositions();
+    }
+
+    private void ReorganizeHeldCardPositions()
+    {
+        var pos = cardXStartPos;
+        var xOffset = 0f;
+        for (int i = 0; i < currentHandCount; i++)
+        {
+            var card = heldCards[i];
+            if (card.transform.parent == transform) { continue; }
+            card.transform.position = this.transform.position + new Vector3(pos + xOffset, 0f, 0f);
+            card.transform.parent = transform;
+            card.SlotedPosition = heldCards[i].transform.position;
+            card.HighLightedEvent.AddListener(SetHighLightedCard);
+            xOffset += cardXOfffest;
+        }
+    }
+
+    private bool DrawCard(int drawIndex)
+    {
+        var retval = false;
+        var card = cardDeck.CreateCard();
+        if (card)
+        {
+            heldCards[drawIndex] = card;
+            retval = true;
+        }
+        return retval;
+    }
+
+    private void SetHighLightedCard(CardBase card)
+    {
+        HighLightedCard = card;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+   
 }
