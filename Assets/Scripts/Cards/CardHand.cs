0249c280c55952ac884b6ab93ef391daf7a49e7c
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-03 16:15:44 +0100

    added highlighting in hand and added way to update targeting line

--- Assets/Scripts/Cards/CardHand.cs
+++ Assets/Scripts/Cards/CardHand.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CardHand : MonoBehaviour
@@ -15,6 +16,10 @@ public class CardHand : MonoBehaviour
     private CardBase[] heldCards = new CardBase[5];
     private CardBase highLightedCard = null;
     private CardBase selectedCard = null;
+    private LineRenderer targetingLine = null;
+
+    private Ray ray;
+    private RaycastHit hit;
 
     //TODO needs a list of cards,
     //a way of highlighting cards that the mouse pases over,
@@ -25,6 +30,28 @@ public class CardHand : MonoBehaviour
     void Start()
     {
         DrawCardsPhase();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            HoverOverCardInHand();
+            UpdateTargetingLine();
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SelectCardInHand();
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            DeSelectCardInHand();
+        }
     }
 
     private void DrawCardsPhase()
@@ -50,7 +77,6 @@ public class CardHand : MonoBehaviour
             card.transform.position = this.transform.position + new Vector3(pos + xOffset, 0f, 0f);
             card.transform.parent = transform;
             card.SlotedPosition = heldCards[i].transform.position;
-            card.HighLightedEvent.AddListener(SetHighLightedCard);
             xOffset += cardXOfffest;
         }
     }
@@ -67,6 +93,74 @@ public class CardHand : MonoBehaviour
         return retval;
     }
 
+
+   
+
+    private void HoverOverCardInHand()
+    {
+        if (selectedCard) { return; }
+
+        var entity = hit.collider.gameObject;
+        if (!entity) { return; }
+
+        if (highLightedCard && entity != highLightedCard.gameObject)
+        {
+            SetHighLightedCard(
+               null,
+               highLightedCard.SlotedPosition,
+               highLightedCard.OriginalSize);
+            return;
+        }
+
+        var card = entity.GetComponent<CardBase>();
+        if (!card) { return; }
+
+
+        if (heldCards.Contains(card))
+        {
+            SetHighLightedCard(
+                card,
+                card.SlotedPosition + new Vector3(0f, 0f, 2f),
+                card.OriginalSize + new Vector3(0.2f, 0.2f, 0.2f));
+        }
+    }
+
+  
+
+    private void DeSelectCardInHand()
+    {
+        if (selectedCard)
+        {
+            SetCardPosAndSize(selectedCard, selectedCard.SlotedPosition, selectedCard.OriginalSize);
+            selectedCard.TryDoDeSelectActions(null);
+            selectedCard = null;
+            highLightedCard = null;
+        }
+    }
+
+    private void SelectCardInHand()
+    {
+        if (highLightedCard && selectedCard == null)
+        {
+            selectedCard = highLightedCard;
+            selectedCard.TryDoSelectActions(null);
+        }
+    }
+
+    private void UpdateTargetingLine()
+    {
+        if (!selectedCard) { return; }
+
+        if (!targetingLine)
+        {
+            targetingLine = selectedCard.GetComponent<LineRenderer>();
+            return;
+        }
+
+        targetingLine.SetPosition(1, hit.point);
+
+    }
+
     private void SetHighLightedCard(CardBase card, Vector3 posToSet, Vector3 sizeToSet)
     {
         if (highLightedCard && card == null)
@@ -79,29 +173,6 @@ public class CardHand : MonoBehaviour
         }
 
         highLightedCard = card;
-    }
-
-    public bool IsThereACardSelection()
-    {
-        return selectedCard != null;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            if (highLightedCard && selectedCard == null) { selectedCard = highLightedCard; }
-        }
-        else if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            if (selectedCard)
-            {
-                SetCardPosAndSize(selectedCard, selectedCard.SlotedPosition, selectedCard.OriginalSize);
-                selectedCard = null;
-                highLightedCard = null;
-            }
-        }
     }
 
     private void SetCardPosAndSize(CardBase card, Vector3 posToSet, Vector3 sizeToSet)
