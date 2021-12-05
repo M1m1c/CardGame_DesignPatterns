656ea7adf931db007b894c8af463081eb7ed5dde
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-05 14:40:18 +0100

    Added new damage card value action

---
+++ Assets/Scripts/Cards/CardActions/DamageCardValue.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Action_DamageCardValue", menuName = "Actions/Action_DamageCardValue")]
+public class DamageCardValue : CardAction
+{
+    public override void DoCardAction(CardBase owner, GameObject target)
+    {
+        if (!owner) { return; }
+
+        var targetCard = target.GetComponent<CardPlayable>();
+        if (!targetCard) { return; }
+
+        var parent = owner.transform.parent;
+        if (!parent) { return; }
+
+        var hand = parent.GetComponent<CardHand>();
+        if (!hand) { return; }
+
+        targetCard.ChangeCardValue(-owner.CardValue);
+        hand.RemoveCard(owner);
+        Destroy(owner.gameObject);
+    }
+}
