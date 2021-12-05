ec65b6b4a84021604994090b6e6b2dfa82869576
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-05 14:40:03 +0100

    removed unused usings

--- Assets/Scripts/Cards/CardActions/AddCardToPlayArea.cs
+++ Assets/Scripts/Cards/CardActions/AddCardToPlayArea.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Action_AddCardToPlayArea", menuName = "Actions/Action_AddCardToPlayArea")]
