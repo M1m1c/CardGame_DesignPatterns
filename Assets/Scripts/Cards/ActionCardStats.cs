e03b69e86480936570309ce4d0cd550e25fe2b0a
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-05 14:59:55 +0100

    Created action card stats and made all action cards scriptable objects use that instead

---
+++ Assets/Scripts/Cards/ActionCardStats.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewActionCardStats", menuName = "Cards/ActionCardStats")]
+public class ActionCardStats : CardStatsBase
+{
+    public int minValue = 0;
+    public int MaxValue = 0;
+}
