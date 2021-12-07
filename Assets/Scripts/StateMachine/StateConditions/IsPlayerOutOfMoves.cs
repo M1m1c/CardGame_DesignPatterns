1e58cf6ce0eb2848048a63e69147b262a6a9fe36
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-07 16:12:06 +0100

    added is player out of moves link condition

---
+++ Assets/Scripts/StateMachine/StateConditions/IsPlayerOutOfMoves.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Condition_IsPlayerOutOfMoves", menuName = "Conditions/Condition_IsPlayerOutOfMoves")]
+public class IsPlayerOutOfMoves : LinkConditionBase
+{
+    public override bool CheckCondition(GameMaster owner, CardHolder target)
+    {
+        var retval = false;
+        if (owner && target)
+        {
+            var cantPlayHeroCard = owner.HeroPlayedThisTurn || !target.DoesHeldCardsContainType(CardType.Hero);
+            if (owner.ActionCardAllowence.Count == 0 && cantPlayHeroCard)
+            {
+                //Player turn is over, transition to ai turn
+                Debug.Log("Player turn over");
+                retval = true;
+            }
+        }
+
+        return retval;
+    }
+
+}
