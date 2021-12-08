b076df8d1561ca93bb6e1a02ef9b99098e61f36d
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-08 15:46:17 +0100

    Added color setter to targeting line action

--- Assets/Scripts/Cards/CardActions/AddTargetingLine.cs
+++ Assets/Scripts/Cards/CardActions/AddTargetingLine.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Action_AddTargetingLine", menuName = "Actions/Action_AddTargetingLine")]
 public class AddTargetingLine : CardAction
 {
+    public Color LineColor;
+    public Material LineMaterial;
+
     public override void DoAction(CardBase owner, GameObject target)
     {
         if (!owner) { return; }
         if (owner.GetComponent<LineRenderer>()) { return; }
         var lineRenderer=owner.gameObject.AddComponent<LineRenderer>();
-        lineRenderer.positionCount = 2;
+        lineRenderer.positionCount = 2;    
         lineRenderer.SetPosition(0, owner.transform.position);
         lineRenderer.SetPosition(1, owner.transform.position);
+        lineRenderer.material = LineMaterial;
+        lineRenderer.startColor = LineColor;
+        lineRenderer.endColor = LineColor;
     }
 }
