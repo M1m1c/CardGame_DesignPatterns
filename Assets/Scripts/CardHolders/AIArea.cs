930a2a042899b601988b7a2b939768707738d55a
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-08 15:47:05 +0100

    MadeAi area draw cards from its deck and added subject OnUnitDeath

--- Assets/Scripts/CardHolders/AIArea.cs
+++ Assets/Scripts/CardHolders/AIArea.cs
@@ -1,20 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AIArea : CardArea
 {
+    public Subject<CardHolder> OnUnitDeath { get; private set; } = new Subject<CardHolder>();
+
+    public int cardsToDrawPerRound { get; set; } = 3;
+
+    public CardDeck cardDeck;
+
+    private void Start()
+    {
+        AddUnitsToArea();
+    }
+
+    private void AddUnitsToArea()
+    {
+        while (currentHeldCount < cardsToDrawPerRound)
+        {
+            var card = cardDeck.CreateCard();
+            AddCard(card);
+        }
+    }
+
     public override void AddCard(CardBase card)
     {
         if (!card) { return; }
         if (currentHeldCount >= heldCards.Length) { return; }
         cardXStartPos = cardXPosStarts[currentHeldCount];
-        heldCards[currentHeldCount] = card;
+        heldCards[Array.IndexOf(heldCards, null)] = card;
         currentHeldCount++;
         ReorganizeHeldCardPositions();
     }
-    public override void RemoveCard()
+    public override void RemoveCard(CardBase card)
     {
-        //base.RemoveCard();
+        base.RemoveCard(card);
+        OnUnitDeath.Notify(this);
     }
 }
