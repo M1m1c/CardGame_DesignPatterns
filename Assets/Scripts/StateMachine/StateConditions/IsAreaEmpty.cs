777bdd42bc5891907d01dd7b996c1690c1b3c5dd
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-08 22:11:24 +0100

    Renamed condition to is area empty

--- Assets/Scripts/StateMachine/StateConditions/IsAreaEmpty.cs
+++ Assets/Scripts/StateMachine/StateConditions/IsAreaEmpty.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
 
 
-[CreateAssetMenu(fileName = "Condition_IsOutOfEnemies", menuName = "Conditions/Condition_IsOutOfEnemies")]
-public class IsOutOfEnemies : LinkConditionBase
+[CreateAssetMenu(fileName = "Condition_IsAreaEmpty", menuName = "Conditions/Condition_IsAreaEmpty")]
+public class IsAreaEmpty : LinkConditionBase
 {
     public override bool CheckCondition(GameMaster owner, CardHolder target)
     {
         var retval = false;
         if (owner && target)
         {
-            if(!(target is CardHand)) 
+            if((target is CardArea)) 
             {
                 if (target.GetCurrentHeldCards() <= 0) { retval = true; }
             }                         
