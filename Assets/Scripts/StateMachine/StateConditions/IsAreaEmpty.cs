9cc7043d386319b73e3df0f3a6c3ec2efcee0ab8
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-09 16:38:59 +0100

    removed empty lines

--- Assets/Scripts/StateMachine/StateConditions/IsAreaEmpty.cs
+++ Assets/Scripts/StateMachine/StateConditions/IsAreaEmpty.cs
@@ -17,5 +17,4 @@ public class IsAreaEmpty : LinkConditionBase
 
         return retval;
     }
-
 }
--- Assets/Scripts/StateMachine/StateConditions/IsPlayerOutOfMoves.cs
+++ Assets/Scripts/StateMachine/StateConditions/IsPlayerOutOfMoves.cs
@@ -30,5 +30,4 @@ public class IsPlayerOutOfMoves : LinkConditionBase
 
         return retval;
     }
-
 }
